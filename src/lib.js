03c14b4b73c2c810d2844ca519e74642cb1e5cc6
Artem Shubovych <shybovycha@gmail.com>
2024-07-22 16:16:35 +1000

    Modernize the code
    Add build workflow
    
    Split code into smaller functions
    
    Revert "Split code into smaller functions"
    
    This reverts commit d2b9240a59553d34d7b8f8fde0abe0c64cffdeb4.
    
    Fix imports
    
    Use raw nodejs
    
    Remove node: prefix from imports
    
    Split processing into two promises and use @actions/glob
    
    Add missing module
    
    Add failure when no artifacts are matched
    
    Tidy up log messages
    
    Rename function for cleaner meaning
    
    Use for-await expression
    
    Use glob v11
    
    Roll back glob since it does not support ignore patterns as easily as @actions/glob
    
    Update README
    
    Rollback action.yml changes for contribution
    
    Update README re. nvm
    
    Rollback action name
    
    Signed-off-by: Artem Shubovych <shybovycha@gmail.com>

--- src/lib.js
+++ src/lib.js
@@ -1,77 +1,92 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const glob = require('@actions/glob');
 
 const path = require('path');
-const fs = require('fs');
-const { pathToFileURL } = require('url');
-const GetRelease = require('./get-release')
-const glob = require('glob')
+const fsPromises = require('fs/promises');
 
-async function run() {
+const getReleaseURL = async (octokit, context) => {
+  // Get owner and repo from context of payload that triggered the action
+  const { owner, repo } = context.repo;
+
+  // Get the tag name from the triggered action
+  const tagName = context.ref;
+
+  // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
+  const tag = tagName.replace('refs/tags/', '');
+
+  // Get a release from the tag name
+  // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
+  // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
+  const getReleaseResponse = await octokit.repos.getReleaseByTag({
+    owner,
+    repo,
+    tag
+  });
+
+  const uploadURL = getReleaseResponse.data.upload_url;
+
+  return uploadURL;
+};
+
+const run = async () => {
   try {
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
     const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
-    const getRelease = new GetRelease(octokit, github.context)
 
-    const uploadUrl = await getRelease.getURL()
+    const uploadUrl = await getReleaseURL(octokit, github.context);
 
     // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-    const assetPathsSt = core.getInput('asset_paths', { required: true });
+    const assetPathsStr = core.getInput('asset_paths', { required: true });
+    const globber = await glob.create(assetPathsStr);
+    const paths = await globber.glob();
 
-    const assetPaths = JSON.parse(assetPathsSt)
-    if(!assetPaths || assetPaths.length == 0) {
-      core.setFailed("asset_paths must contain a JSON array of quoted paths");
-      return
+    if (paths.length === 0) {
+      core.setFailed(`Could not find any artifacts with paths: ${assetPathsStr}`);
+      return;
     }
 
-    let paths = []
-    for(let i = 0; i < assetPaths.length; i++) {
-      let assetPath = assetPaths[i];
-      if(assetPath.indexOf("*") > -1) {
-        const files = glob.sync(assetPath,{ nodir: true })
-        for (const file of files) {
-            paths.push(file)
-        }
-      }else {
-        paths.push(assetPath)
-      }
-    }
+    core.debug(`Expanded paths: ${paths}`);
 
-    core.debug(`Expanded paths: ${paths}`)
+    const downloadURLs = [];
 
-    downloadURLs = []
-    for(let i = 0; i < paths.length; i++) {
-      let asset = paths[i];
-
+    for await (const assetPath of paths) {
       // Determine content-length for header to upload asset
-      const contentLength = filePath => fs.statSync(filePath).size;
-      const contentType = "binary/octet-stream"
+      const stats = await fsPromises.stat(assetPath);
+      const contentLength = stats.size;
+      const contentType = 'binary/octet-stream';
+
       // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
-      const headers = { 
-        'content-type': contentType, 
-        'content-length': contentLength(asset)
+      const headers = {
+        'content-type': contentType,
+        'content-length': contentLength,
       };
-  
-      const assetName = path.basename(asset)
-      console.log(`Uploading ${assetName}`)
+
+      const assetName = path.basename(assetPath);
+
+      const content = await fsPromises.readFile(assetPath);
+
+      const request = {
+        url: uploadUrl,
+        headers,
+        name: assetName,
+        data: content,
+      };
+
+      core.info(`Uploading asset ${request.name}`);
 
       // Upload a release asset
       // API Documentation: https://developer.github.com/v3/repos/releases/#upload-a-release-asset
       // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset
-      const uploadAssetResponse = await octokit.repos.uploadReleaseAsset({
-        url: uploadUrl,
-        headers,
-        name: assetName,
-        data: fs.readFileSync(asset)
-      });
-  
+      const uploadAssetResponse = await octokit.repos.uploadReleaseAsset(request);
+
       // Get the browser_download_url for the uploaded release asset from the response
       const {
         data: { browser_download_url: browserDownloadUrl }
       } = uploadAssetResponse;
-  
+
       // Set the output variable for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-      downloadURLs.push(browserDownloadUrl)
+      downloadURLs.push(browserDownloadUrl);
     }
 
     core.setOutput('browser_download_urls', JSON.stringify(downloadURLs));
