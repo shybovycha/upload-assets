f207ca944d35416c8c895a2114c9c85a614ed151
Artem Shubovych <shybovycha@gmail.com>
2024-07-22 16:16:35 +1000

    Modernize the code

--- Makefile
+++
@@ -1,4 +0,0 @@
-.PHONY: dist
-
-dist:
-	ncc build src
--- src/lib.js
+++ src/lib.js
@@ -1,78 +1,98 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const path = require('path');
-const fs = require('fs');
-const { pathToFileURL } = require('url');
-const GetRelease = require('./get-release')
-const glob = require('glob')
+const path = require('node:path');
+const fsPromises = require('node:fs/promises');
 
-async function run() {
+const getReleaseURL = async (octokit, context) => {
+  // Get owner and repo from context of payload that triggered the action
+  const { owner, repo } = context.repo;
+
+  // Get the tag name from the triggered action
+  const tagName = context.ref;
+
+  // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
+  const tag = tagName.replace('refs/tags/', '');
+
+  // Get a release from the tag name
+  // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
+  // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
+  const getReleaseResponse = await octokit.repos.getReleaseByTag({
+    owner,
+    repo,
+    tag
+  });
+
+  const uploadURL = getReleaseResponse.data.upload_url;
+
+  return uploadURL;
+};
+
+const run = async () => {
   try {
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
     const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
-    const getRelease = new GetRelease(octokit, github.context)
 
-    const uploadUrl = await getRelease.getURL()
+    const uploadUrl = await getReleaseURL(octokit, github.context);
 
     // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
     const assetPathsSt = core.getInput('asset_paths', { required: true });
 
-    const assetPaths = JSON.parse(assetPathsSt)
-    if(!assetPaths || assetPaths.length == 0) {
-      core.setFailed("asset_paths must contain a JSON array of quoted paths");
-      return
+    const assetPaths = JSON.parse(assetPathsSt);
+
+    if (!assetPaths || assetPaths.length == 0) {
+      core.setFailed('asset_paths must contain a JSON array of quoted paths');
+      return;
     }
 
-    let paths = []
-    for(let i = 0; i < assetPaths.length; i++) {
-      let assetPath = assetPaths[i];
-      if(assetPath.indexOf("*") > -1) {
-        const files = glob.sync(assetPath,{ nodir: true })
-        for (const file of files) {
-            paths.push(file)
+    const paths = await Promise.all(
+      assetPaths.flatMap(async (assetPath) => {
+        if (assetPath.includes('*')) {
+          return await fsPromises.glob(assetPath, { nodir: true });
+        } else {
+          return assetPath;
         }
-      }else {
-        paths.push(assetPath)
-      }
-    }
+      })
+    );
 
-    core.debug(`Expanded paths: ${paths}`)
+    core.debug(`Expanded paths: ${paths}`);
 
-    downloadURLs = []
-    for(let i = 0; i < paths.length; i++) {
-      let asset = paths[i];
+    const downloadURLs = await Promise.all(
+      paths.map(async (asset) => {
+        // Determine content-length for header to upload asset
+        const stats = await fsPromises.stat(asset);
+        const contentLength = stats.size;
+        const contentType = 'binary/octet-stream';
 
-      // Determine content-length for header to upload asset
-      const contentLength = filePath => fs.statSync(filePath).size;
-      const contentType = "binary/octet-stream"
-      // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
-      const headers = { 
-        'content-type': contentType, 
-        'content-length': contentLength(asset)
-      };
-  
-      const assetName = path.basename(asset)
-      console.log(`Uploading ${assetName}`)
+        // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
+        const headers = {
+          'content-type': contentType,
+          'content-length': contentLength,
+        };
 
-      // Upload a release asset
-      // API Documentation: https://developer.github.com/v3/repos/releases/#upload-a-release-asset
-      // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset
-      const uploadAssetResponse = await octokit.repos.uploadReleaseAsset({
-        url: uploadUrl,
-        headers,
-        name: assetName,
-        data: fs.readFileSync(asset)
-      });
-  
-      // Get the browser_download_url for the uploaded release asset from the response
-      const {
-        data: { browser_download_url: browserDownloadUrl }
-      } = uploadAssetResponse;
-  
-      // Set the output variable for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-      downloadURLs.push(browserDownloadUrl)
-    }
+        const assetName = path.basename(asset);
+
+        core.info(`Uploading asset ${assetName}`);
+
+        // Upload a release asset
+        // API Documentation: https://developer.github.com/v3/repos/releases/#upload-a-release-asset
+        // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset
+        const uploadAssetResponse = await octokit.repos.uploadReleaseAsset({
+          url: uploadUrl,
+          headers,
+          name: assetName,
+          data: fs.readFileSync(asset),
+        });
+
+        // Get the browser_download_url for the uploaded release asset from the response
+        const {
+          data: { browser_download_url: browserDownloadUrl }
+        } = uploadAssetResponse;
+
+        // Set the output variable for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
+        return browserDownloadUrl;
+      })
+    );
 
     core.setOutput('browser_download_urls', JSON.stringify(downloadURLs));
   } catch (error) {
